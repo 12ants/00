f88207189e12b6d1226f33c8ce95738a5ba15875
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-04-27 11:40:10 +0200

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 ## Install-script for Ubuntu/Debian systems
 ## visit https://12ants.github.io for credits
-rootgit=github.com/12ants/00;
+rootgit=12ants.github.io/00;
 if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi; ## ROOT-CHECK
 
 ## ADDING COLOR-CODES -- (Need to run inside other command.)
