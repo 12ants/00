5155f768c0528e9438dc1645921c47967692c85a
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-05-17 06:27:11 +0200

    Update auto-sudo.sh

--- auto-sudo.sh
+++ auto-sudo.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi; ## ROOT-CHECK
 
-alias "ipnet"="hostname -I | head -c 13"
-alias "ippublic"="dig +short myip.opendns.com @resolver1.opendns.com"
-alias "ipports"="lsof -i -P -n"
-tput setaf 11
-ipnet
-ippublic
+alias "ipnet"="tput setaf 1; hostname -I | head -c 13; tput sgr0; echo;"
+alias "ippublic"="tput setaf 2; dig +short myip.opendns.com @resolver1.opendns.com; tput sgr0;"
+alias "ipports"="tput setaf 6; lsof -i -P -n; tput sgr0;"
+sleep 1;
+echo -e "  -- Open ports:"
 ipports
-tput setaf 15
+echo -e "  -- Network IP:"; ipnet; echo -e "  -- Network IP:"; ippublic;
+
 touch ~/.bash_aliases;
-touch /.bash_aliases;
+touch /root/.bash_aliases;
 
 #### USER
 echo '
@@ -18,7 +18,7 @@ echo '
 alias "ipnet"="hostname -I | head -c 13"
 alias "ippublic"="dig +short myip.opendns.com @resolver1.opendns.com"
 alias "ipports"="lsof -i -P -n"
-## USERNAME
+## USERNAME 8888
 PS1="\[\e[0;38;5;23m\]$? \[\e[0;2m\]/ \[\e[0;38;5;30m\]$(ipnet) \[\e[0;2m\]/ \[\e[0;38;5;31m\]\u \[\e[0;2m\]/ \[\e[0;38;5;36m\]\w \[\e[0m\]> \[\e[0m\]"'
 >> ~/.bash_aliases;
 
@@ -32,20 +32,7 @@ alias "ipports"="lsof -i -P -n"
 ## USERNAME
 PS1="\[\e[0;38;5;23m\]$? \[\e[0;2m\]/ \[\e[0;38;5;30m\]$(ipnet) \[\e[0;2m\]/ \[\e[0;38;5;31m\]\u \[\e[0;2m\]/ \[\e[0;38;5;36m\]\w \[\e[0m\]> \[\e[0m\]"'
 >> /root/.bash_aliases;
-
-## auto root for for admins
-echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer;
-
-
-## ROOT AUTOCOMPLETE
-echo "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
-    . /etc/bash_completion
-fi
-" >> /root/.bashrc;
-
-
-
-#### TMP
+## same mmmmmmmmmmmmmmmmmmmmmm
 echo '
 ## ALIAS
 alias "ipnet"="hostname -I | head -c 13"
@@ -53,4 +40,13 @@ alias "ippublic"="dig +short myip.opendns.com @resolver1.opendns.com"
 alias "ipports"="lsof -i -P -n"
 ## USERNAME
 PS1="\[\e[0;38;5;23m\]$? \[\e[0;2m\]/ \[\e[0;38;5;30m\]$(ipnet) \[\e[0;2m\]/ \[\e[0;38;5;31m\]\u \[\e[0;2m\]/ \[\e[0;38;5;36m\]\w \[\e[0m\]> \[\e[0m\]"'
->> /.bash_aliases;
+>> /root/.bashrc;
+
+## auto root for for admins
+echo "%sudo ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/10-installer;
+## ROOT AUTOCOMPLETE
+echo "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
+fi
+" >> /root/.bashrc;
+
