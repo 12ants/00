7744913c69e76cad595b9579dbad85a87bd8cc9c
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-05-17 07:02:57 +0200

    Update auto-sudo.sh

--- auto-sudo.sh
+++ auto-sudo.sh
@@ -1,6 +1,4 @@
-#/bin/bash
-if [ $UID != 0 ]; then echo -e " \n\n\t This script must be run as root... try command: [ sudo -s ] \n\n " 1>&2; exit 1; fi; ## ROOT-CHECK
-
+#!/bin/bash
 alias "ipnet"="tput setaf 1; hostname -I | head -c 13; tput sgr0; echo;"
 alias "ippublic"="tput setaf 2; dig +short myip.opendns.com @resolver1.opendns.com; tput sgr0;"
 alias "ipports"="tput setaf 6; lsof -i -P -n; tput sgr0;"
@@ -8,10 +6,8 @@ sleep 1;
 echo -e "  -- Open ports:"
 ipports
 echo -e "  -- Network IP:"; ipnet; echo -e "  -- Network IP:"; ippublic;
-
 touch ~/.bash_aliases;
 touch /root/.bash_aliases;
-
 #### USER
 echo '
 ## ALIAS
@@ -21,7 +17,6 @@ alias "ipports"="lsof -i -P -n"
 ## USERNAME 8888
 PS1="\[\e[0;38;5;23m\]$? \[\e[0;2m\]/ \[\e[0;38;5;30m\]$(ipnet) \[\e[0;2m\]/ \[\e[0;38;5;31m\]\u \[\e[0;2m\]/ \[\e[0;38;5;36m\]\w \[\e[0m\]> \[\e[0m\]"'
 >> ~/.bash_aliases;
-
 ################
 #### ROOT
 echo '
@@ -49,4 +44,4 @@ echo "if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
     . /etc/bash_completion
 fi
 " >> /root/.bashrc;
-
+echo " HELLO"
