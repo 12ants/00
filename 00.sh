f68a00c64bbaafcc988843c7d0a5cee127d02047
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-18 20:08:35 +0200

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -130,6 +130,7 @@ else echo "OK"; fi; cd $inst;
 #######################
 ## BBASH - DONE #######
 #######################
+
 if [ $login == y ]; then echo "installing login-screen";
 wget -O 12login.sh $rootgit/login.sh; bash 12login.sh;
 else echo "OK"; fi; cd $inst;
