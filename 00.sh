2f4028ae962859099b0984ac7f3c1590db1e5aec
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-18 20:10:10 +0200

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -157,7 +157,7 @@ else echo "OK"; fi; cd $inst;
 
 ##
 #######################
-## CLO - DONE #######
+## CLOUDPANEL - DONE #######
 #######################
 ##
 if [ $cpr == y ]; then echo "  -- installing cloudpanel.. ";
