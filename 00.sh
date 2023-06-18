a786f12335ec2164b26efd33243fcc3bffd79efe
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-18 20:25:16 +0200

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -11,7 +11,8 @@ ippub="dig +short myip.opendns.com @resolver1.opendns.com"
 ipa="echo ---- $blue Network IP:$cyan ; $ipnet; ---- $purple Public  IP:$cyan ; $ippublic;"
 alias ipa="echo ---- $blue Network IP:$cyan ; $ipnet; ---- $purple Public  IP:$cyan ; $ippublic;"
 
-echo -e 'alias ipa="echo ---- $blue Network IP:$cyan$ipnet
+echo -e 'alias ipa="echo 
+---- $blue Network IP:$cyan$ipnet
 ---- $purple Public  IP:$cyan ; $ippublic;"; ipa'
 
 
