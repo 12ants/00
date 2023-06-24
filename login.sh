fb6ed94e74e5196d610d16f5f459be199bc21dcd
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 19:46:52 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -8,7 +8,7 @@ systemctl set-default multi-user.target
 systemctl set-default graphical.target
 
 
-read 
+read -
 ## Message before login
 
 ## echo "Hello login" >> /etc/issue /etc/ssh/sshd-banner
