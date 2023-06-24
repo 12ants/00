6c738fa02413aa80ef4899efc382c8e1fb08c6cc
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 19:55:54 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -17,7 +17,6 @@ fi
 read -n1 -ep "  --  Choose default login screen [T]erminal or [G]raphical?: " "yn";
 if [ "$yn" != "${yn#[Gg]}" ];
 then echo G;
-systemctl set-default multi-user.target
 fi
 
 
