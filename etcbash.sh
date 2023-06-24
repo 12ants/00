a74581e9937550f41d2dcf0384d16edc26225926
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 19:32:58 +0200

    Update etcbash.sh

--- etcbash.sh
+++ etcbash.sh
@@ -5,8 +5,6 @@ case $- in
       *) return;;
 esac
 HISTCONTROL=ignoreboth
-
-# append to the history file, don't overwrite it
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
