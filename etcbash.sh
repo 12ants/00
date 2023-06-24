5bedf9c50f0216075cb77856bd4ef4a4798562ac
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 19:33:11 +0200

    Update etcbash.sh

--- etcbash.sh
+++ etcbash.sh
@@ -8,7 +8,6 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 HISTSIZE=1000
 HISTFILESIZE=2000
-
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
