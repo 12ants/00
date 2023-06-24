a96d795f9be16e701194ddd1b75f6e86e9cdb336
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 20:01:19 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -17,7 +17,7 @@ fi
 
 
 alias loginscreen='read -n1 -ep "  --  Choose default login screen [T]erminal or [G]raphical?: " "yn";
-if [ "$lscreen" != "${loginscreen#[Gg]}" ];
+if [ "$lscreen" != "${lscreen#[Gg]}" ];
 then echo Graphical; systemctl set-default graphical.target;
 else echo Terminal; systemctl set-default multi-user.target;
 fi
