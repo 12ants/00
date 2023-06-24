0d9e3675dda1caf1e32634f28f68f54bf80ce50a
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 19:40:54 +0200

    Update 00.sh

--- 00.sh
+++ 00.sh
@@ -137,7 +137,7 @@ else echo "OK"; fi; cd $inst;
 
 ##
 #######################
-## WEBMIN -      ######
+## WEBMIN        ######
 #######################
 ##
 if [ $webmin == y ]; then echo "installing webmin";
