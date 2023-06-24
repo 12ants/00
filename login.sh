2c1d4d680f040b7ca3fd5b4c47512db7dc19416b
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-24 19:48:59 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -8,7 +8,7 @@ systemctl set-default multi-user.target
 systemctl set-default graphical.target
 
 
-read -n1 -ep "  --  Choose default login screen [t]erminal or [g]raphical?: " "login"
+read -n1 -ep "  --  Choose default login screen [t]erminal or [g]raphical?: " "loginscreen"
 ## Message before login
 
 ## echo "Hello login" >> /etc/issue /etc/ssh/sshd-banner
