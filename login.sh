4381ae2c5d7d4d3855b7f22da611f1ec15aae2c9
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-26 22:32:21 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -37,7 +37,7 @@ echo '
 if [ "${PS1-}" ]; then
   if [ "${BASH-}" ] && [ "$BASH" != "/bin/sh" ]; then
     # The file bash.bashrc already sets the default PS1.
-    # PS1='\h:\w\$ '
+    # PS1="\h:\w\$ "
     if [ -f /etc/bash.bashrc ]; then
       . /etc/bash.bashrc
     fi
