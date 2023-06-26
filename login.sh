0f74e2820367ac53361bb9609ab50d12b161db3b
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-26 22:43:12 +0200

    Update login.sh

--- login.sh
+++ login.sh
@@ -35,14 +35,7 @@ mv /etc/profile /etc/profile~bu
 
 /etc/profile
 ##
-echo 'alias loginscreen="
-read -n1 -ep "  --  Choose default login screen, [G]raphical or  [t]erminal?: " "yn";
-if [ "$lscreen" != "${lscreen#[Gg]}" ];
-then echo Graphical; systemctl set-default graphical.target;
-else echo Terminal; systemctl set-default multi-user.target;
-fi
-loginscreen"'
->> /etc/profile
+
 ##
 
 echo "
