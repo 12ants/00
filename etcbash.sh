61cbe1cab53dc512069595a7d8d0fe3c7d9397a5
Leon Ljunghorn <leonljunghorn@gmail.com>
2023-06-26 23:15:38 +0200

    Update etcbash.sh

--- etcbash.sh
+++ etcbash.sh
@@ -94,7 +94,7 @@ if ! shopt -oq posix; then
     . /etc/bash_completion
   fi
 fi
-alias "2222"=""
+alias "2222"="get"
 alias "oooo"="tput setaf 7 dim; fortune; tput sgr0;"
 alias "rot"="sudo -s"
 alias "ipnet"="hostname -I | head -c 13";
